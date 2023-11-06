14245d0925ce67b11437ddc258f91c81226e12b6
Joyce Jiang <Joyce.Jiang@serko.com>
2023-11-07 11:38:17 +1300

    add allowed host

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-cj5^vl-f7ekk+=^66z#to=_odw-n3_!k745euocyxuyos8!qqx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cindyjiang959.pythonanywhere.com']
 
 
 # Application definition
